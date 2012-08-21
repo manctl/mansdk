d72285f0253efa503a0c781d4bd8c64dc153b1c1
Nicolas Tisserand <nt@manctl.com>
2012-08-20 20:37:44 -0700

    Add private/public url helper scripts.

---
+++ .set-public-urls.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+if test -z "$WINDIR"; then
+    case `uname` in
+        Darwin)
+            SED_I="-i ''"
+            ;;
+
+        linux)
+            SED_I="-i"
+            ;;
+    esac
+else
+    # Windows
+    SED_I="-i"
+fi
+
+sed $SED_I \
+-e 's!git@github.com:manctl/!git://github.com/manctl/!' \
+-e 's!git@bitbucket.org:manctl/!https://bitbucket.org/manctl/!' \
+.gitmodules
+
+git submodule sync
+git submodule update
